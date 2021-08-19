ef6b4f7da7f066b7d2edf4de138445d56686b8a7
Shaeen Singh <shaeenkevinsingh@gmail.com>
2021-08-19 10:24:03 +0200

    Adding functionality to sentence builder form

--- app/pages/builder/builderCtrl.js
+++ app/pages/builder/builderCtrl.js
@@ -5,6 +5,87 @@
     controller: [
       function () {
         var $ctrl = this
+
+        $ctrl.wordTypes = []
+        $ctrl.words = []
+        $ctrl.selectedWordType = null
+        $ctrl.selectedWord = null
+        $ctrl.isWordTypeSelected = false
+        $ctrl.currentSentence = ''
+
+        $ctrl.onWordTypeChange = function () {
+          $ctrl.isWordTypeSelected = true
+
+          getWordsByType()
+        }
+
+        /**
+         * $ctrl.onWordChange()
+         *
+         * TODO: Remove function when backend is complete
+         * Using this function to see what my object will look
+         * like
+         */
+        $ctrl.onWordChange = function () {
+          console.log($ctrl.selectedWord)
+        }
+
+        $ctrl.addWord = function () {
+          $ctrl.currentSentence += $ctrl.selectedWord.title + ' '
+        }
+
+        $ctrl.postSentence = function () {
+          if (!$ctrl.currentSentence) return
+
+          console.log($ctrl.currentSentence)
+        }
+
+        /**
+         * getWordTypes()
+         *
+         * Fetching all the word types available
+         */
+        function getWordTypes() {
+          $ctrl.wordTypes = [
+            { id: '1', title: 'Verb' },
+            { id: '2', title: 'Noun' },
+            { id: '3', title: 'Adverb' }
+          ]
+        }
+
+        /**
+         * getWordsByType()
+         *
+         * Fetches all the words under a word type which will be use to create a sentence
+         * @returns
+         */
+        function getWordsByType() {
+          if (!$ctrl.selectedWordType) {
+            $ctrl.words = []
+            $ctrl.currentSentence = ''
+            $ctrl.isWordTypeSelected = false
+
+            return
+          }
+
+          $ctrl.words = [
+            { id: '1', title: 'Run' },
+            { id: '2', title: 'Easter' },
+            { id: '3', title: 'accidentally' }
+          ]
+        }
+
+        /**
+         * constructor()
+         *
+         * First function to run in the controller
+         * to fetch required data for the Sentence Builder form
+         */
+        function constructor() {
+          getWordTypes()
+        }
+
+        constructor()
       }
     ]
   })
